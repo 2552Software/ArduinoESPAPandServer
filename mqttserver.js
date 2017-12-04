09fee9a0aead234310e406499ae5997fa5adaf54
Mark S <mark.shavlik@shavlik.us>
2017-12-04 10:31:05 -0600

    Update mqttserver.js

--- mqttserver.js
+++ mqttserver.js
@@ -1,20 +1,7 @@
 var mosca = require('mosca')
 
-var ascoltatore = {
-  type: 'redis',
-  redis: require('redis'),
-  db: 12,
-  port: 6379,
-  return_buffers: true, // to handle binary payloads
-  host: "localhost"
-};
-
 var moscaSettings = {
-  port: 1883,
-  backend: ascoltatore,
-  persistence: {
-    factory: mosca.persistence.Redis
-  }
+  port: 1883
 };
 
 var server = new mosca.Server(moscaSettings);
